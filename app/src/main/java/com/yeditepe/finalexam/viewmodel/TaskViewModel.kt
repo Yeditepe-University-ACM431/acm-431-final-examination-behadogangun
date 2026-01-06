79931239c0d5fba74f256962db5f453e0e279032
Beha Dogangun <90901379+behadogangun@users.noreply.github.com>
2026-01-06 15:15:45 +0300

    Update TaskViewModel.kt

--- app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
+++ app/src/main/java/com/yeditepe/finalexam/viewmodel/TaskViewModel.kt
@@ -1,13 +1,24 @@
 package com.yeditepe.finalexam.viewmodel
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
+import com.yeditepe.finalexam.model.Task
 
 class TaskViewModel : ViewModel() {
 
     // TODO 1: Create a mutable state list of Task objects
     // Initially add at least 2 tasks
+    val tasks = mutableStateListOf(
+        Task(1, "Review MVVM", false),
+        Task(2, "Practice Jetpack Compose", true)
+    )
 
     fun toggleTask(taskId: Int) {
         // TODO 2: Update isCompleted for the given task
+        val index = tasks.indexOfFirst { it.id == taskId }
+        if (index != -1) {
+            // We replace the item with a 'copy' to trigger the state change
+            tasks[index] = tasks[index].copy(isCompleted = !tasks[index].isCompleted)
+        }
     }
 }
