6eb256d375853373f0a18fe23ff575e9634af3c4
Beha Dogangun <90901379+behadogangun@users.noreply.github.com>
2026-01-06 15:15:08 +0300

    Update TaskItemScreen.kt

--- app/src/main/java/com/yeditepe/finalexam/ui/TaskItemScreen.kt
+++ app/src/main/java/com/yeditepe/finalexam/ui/TaskItemScreen.kt
@@ -1,21 +1,28 @@
 package com.yeditepe.finalexam.ui
 
+import android.R
+import android.R.attr.text
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.yeditepe.finalexam.model.Task
 
 @Composable
 fun TaskItemScreen() {
 
     // TODO 1: Create a mutable state to hold completion status (Boolean)
     // Initial value should be false
-
+    var isCompleted by remember { mutableStateOf(false) }
     Column(modifier = Modifier.padding(16.dp)) {
 
         Text(
@@ -25,10 +32,14 @@ fun TaskItemScreen() {
 
         // TODO 2: Show text "Completed" or "Not Completed"
         // depending on completion state
-
+        Text(
+        text = if (isCompleted) "Completed" else "Not Completed",
+        style = MaterialTheme.typography.bodyMedium
+        )
         Button(
             onClick = {
                 // TODO 3: Toggle completion state
+                isCompleted != isCompleted
             }
         ) {
             Text("Change Status")
@@ -40,4 +51,18 @@ fun TaskItemScreen() {
 @Composable
 fun TaskItemPreview() {
     // TODO: Call TaskItemScreen
+    val mockTask = Task(
+        id = 1,
+        title = "Sample Task for Preview",
+        isCompleted = false
+    )
+    TaskItem(
+        task = mockTask,
+        onToggle = { /* No-op for preview */ }
+    )
+}
+
+@Composable
+fun TaskItem(task: Task, onToggle: () -> Unit) {
+    TODO("Not yet implemented")
 }
